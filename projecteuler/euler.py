918fd432c45009a70581203d51c5f8b7be926de1
Timothy DeFreitas <timdef@broadinstitute.org>
2016-09-01 08:52:35 -0400

    Commit problem 5

--- projecteuler/euler.py
+++ projecteuler/euler.py
@@ -52,6 +52,26 @@ def problem3():
 
     return max(_prime_factorization(600851475143))
 
+def problem5():
+    """What is the smallest positive number that is evenly divisible
+    by all the numbers from 1 to 20?"""
+    # To solve this, we enumerate the factors of the number in queston.
+    # First, each of the primes less than 20 must be a factor
+    factors = [2, 3, 5, 7, 11, 13, 17, 19]
+
+    #Next we add factors for each of the numers that can't be factored 
+    # using just unique primes
+    # 4 = 2*2
+    factors.append(2)
+    # 8 = 2*2*2
+    factors.append(2)
+    # 9 = 3*3
+    factors.append(3)
+    #16 = 2*2*2*2
+    factors.append(2)
+
+    return reduce(lambda x,y: x*y, factors)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('problem', type=int)
